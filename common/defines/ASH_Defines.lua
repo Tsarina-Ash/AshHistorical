cca7f0177995b53c846c3b47e3d0a8f2f9d0e086
Ash <chipinterm@gmail.com>
2022-01-26 10:28:51 -0800

    Minor Changes
    LIT color change
    
    Building costs changed for testing purposes
    
    Defines changed & notes written for them & what they do

--- common/defines/ASH_Defines.lua
+++ common/defines/ASH_Defines.lua
@@ -1,32 +1,29 @@
 ---IndustryModifiers
-NDefines.NBuildings.MAX_SHARED_SLOTS = 49
-NDefines.NBuildings.MAX_BUILDING_LEVELS = 49
-NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 10
-NDefines.NProduction.MAX_MIL_FACTORIES_PER_LINE = 300
-NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2
+NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 10 --lower amount of civs allocated per building
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 0.5 --lower based starting production
 ---BegoneClouds
-NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0;
-NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0; 
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0;  --removes fog
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0;  --removes fog
 --SpamMoreDivs
-NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 10000000
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 3000000 --allows player to train more divisions at a time
 --LandCombatChanges
-NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.000
-NDefines.NMilitary.EQUIPMENT_COMBAT_LOSS_FACTOR = 0.6
-NDefines.NMilitary.TRAINING_ATTRITION = 0.03
-NDefines.NMilitary.STRATEGIC_SPEED_BASE = 10.0
+NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.000 --removes equipment gain from combat
+NDefines.NMilitary.EQUIPMENT_COMBAT_LOSS_FACTOR = 0.6 --increased equipment lost from battles
+NDefines.NMilitary.TRAINING_ATTRITION = 0.03 --lowered training attrition
+NDefines.NMilitary.STRATEGIC_SPEED_BASE = 10.0 --planes relocate faster
 ---HigherExperienceCap
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 2000
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 2000
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 2000
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 2000 --higher xp cap
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 2000 --higher xp cap 
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 2000 --higher xp cap
 ---SpeedGoZoomMaybe
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 1000
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 100
-NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
-NDefines.NCountry.EVENT_PROCESS_OFFSET = 25
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.35, 0.15, 0.1, 0.035, 0.0 }
-NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0
-NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0
-NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0
-NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0
-NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0 
-NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 1000 --game doesnt slow down until player is X days behind
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 100 --game doesnt pause down until player is X days behind
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36  --combat log gets deleted after X days
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 25 --how long events can get held for ?
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.35, 0.15, 0.1, 0.035, 0.0 } --game speed (5, 4, 3, 2, 1, paused)
+NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
+NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
+NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
+NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
+NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
+NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0 --score modifier that shows at end of game
